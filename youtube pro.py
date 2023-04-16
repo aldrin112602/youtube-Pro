b4a0d8a7ffb35ad7f0a4056d0822e9b0eec8610c
Aldrin Caballero <aldrincaballero94@gmail.com>
2023-04-17 07:10:55 +0800

    updated

--- youtube pro.py
+++ youtube pro.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from pytube import YouTube, Playlist
 
+
 class Downloader(QDialog):
     def __init__(self, parent=None):
         super(Downloader, self).__init__(parent)
@@ -20,8 +21,7 @@ class Downloader(QDialog):
 
         self.showMenuButton = QPushButton('Download Now')
 
-        self.BrowseButton = QPushButton('Choose path')
-
+        self.BrowseButton = QPushButton('Choose location')
 
         layout = QGridLayout()
         layout.addWidget(self.urlLabel, 0, 0)
@@ -35,10 +35,7 @@ class Downloader(QDialog):
         layout.addWidget(self.BrowseButton, 3, 0)
 
         layout.addWidget(self.showMenuButton, 3, 1)
-        
 
-
-        
         self.setLayout(layout)
         self.setWindowTitle('Video Downloader | Aldrin Caballero')
 
@@ -47,20 +44,16 @@ class Downloader(QDialog):
         self.videoQualityComboBox = QComboBox()
         self.videoQualityComboBoxx = QComboBox()
 
-        self.videoQualityComboBox.addItems(['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
-        self.videoQualityComboBoxx.addItems(['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
+        self.videoQualityComboBox.addItems(
+            ['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
+        self.videoQualityComboBoxx.addItems(
+            ['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
 
-
-        self.BrowseButton.setFixedSize(100, 25)
-        self.showMenuButton.setFixedSize(150,25)
-        self.urlEdit.setFixedSize(150, 25)
-        self.urtwolEdit.setFixedSize(150, 25)
-        self.urthreelEdit.setFixedSize(150, 25)
-
-
-
-
-
+        self.BrowseButton.setFixedSize(200, 50)
+        self.showMenuButton.setFixedSize(300, 50)
+        self.urlEdit.setFixedSize(300, 50)
+        self.urtwolEdit.setFixedSize(300, 50)
+        self.urthreelEdit.setFixedSize(300, 50)
 
         button_style = """
             QPushButton {
@@ -69,13 +62,14 @@ class Downloader(QDialog):
                 font-size:11px;
                 padding: 1rem;
                 font-weight:bold;
-                border-radius: 10px;
-                
+                border-radius: 2px;
+                border-radius: 5px;
             }
 
             QPushButton:hover {
                 background-color: white;
                 color:#f44336;
+                border: 1px solid #222;
             }
         """
 
@@ -83,30 +77,27 @@ class Downloader(QDialog):
             QLabel {
                 font-size:13px;
                 font-weight:bold;
-                color:black;
+                color:green;
             }
     """
-        edit_style ="""
+        edit_style = """
             QLineEdit{
-                background-color:white;
-                border: 2px solid #f44336;
+                padding-left: 20px;
             }
     """
 
         self.BrowseButton.setStyleSheet(button_style)
         self.showMenuButton.setStyleSheet(button_style)
-        
-        
+
         self.urlLabel.setStyleSheet(label_style)
         self.urtwolLabel.setStyleSheet(label_style)
         self.urthreelLabel.setStyleSheet(label_style)
 
-        
         self.urlEdit.setStyleSheet(edit_style)
         self.urtwolEdit.setStyleSheet(edit_style)
         self.urthreelEdit.setStyleSheet(edit_style)
         self.download_Folder = ''
-            
+
     def download(self):
         url = self.urlEdit.text()
         yt = YouTube(url)
@@ -130,9 +121,11 @@ class Downloader(QDialog):
             return
         if self.download_Folder:
             stream.download(output_path=self.download_Folder)
-            QMessageBox.information(self, "Download Completed", "Download has been completed successfully!")
+            QMessageBox.information(
+                self, "Download Completed", "Download has been completed successfully!")
         else:
-            QMessageBox.warning(self, "Error", "Please select download location before download starts")
+            QMessageBox.warning(
+                self, "Error", "Please select download location before download starts")
 
     def downloadPlaylist(self):
         playlist_url = self.urtwolEdit.text()
@@ -140,28 +133,28 @@ class Downloader(QDialog):
         videos = playlist.video_urls
         video_quality = self.videoQualityComboBoxx.currentText()
         for video_url in videos:
-                yt = YouTube(video_url)
-                if video_quality == 'Highest':
-                    stream = yt.streams.get_highest_resolution()
-                elif video_quality == '1080p':
-                    stream = yt.streams.filter(res='1080p').first()
-                elif video_quality == '720p':
-                    stream = yt.streams.filter(res='720p').first()
-                elif video_quality == '480p':
-                    stream = yt.streams.filter(res='480p').first()
-                elif video_quality == '360p':
-                    stream = yt.streams.filter(res='360p').first()
-                elif video_quality == '240p':
-                    stream = yt.streams.filter(res='240p').first()
-                elif video_quality == '144p':
-                    stream = yt.streams.filter(res='144p').first()
-                else:
-                    QMessageBox.warning(self, "Error", "Invalid quality option")
-                    return
-                if self.download_Folder:
-                    stream.download(output_path=self.download_Folder)
-        QMessageBox.information(self, "Download Completed", "Download has been completed successfully!")
-                
+            yt = YouTube(video_url)
+            if video_quality == 'Highest':
+                stream = yt.streams.get_highest_resolution()
+            elif video_quality == '1080p':
+                stream = yt.streams.filter(res='1080p').first()
+            elif video_quality == '720p':
+                stream = yt.streams.filter(res='720p').first()
+            elif video_quality == '480p':
+                stream = yt.streams.filter(res='480p').first()
+            elif video_quality == '360p':
+                stream = yt.streams.filter(res='360p').first()
+            elif video_quality == '240p':
+                stream = yt.streams.filter(res='240p').first()
+            elif video_quality == '144p':
+                stream = yt.streams.filter(res='144p').first()
+            else:
+                QMessageBox.warning(self, "Error", "Invalid quality option")
+                return
+            if self.download_Folder:
+                stream.download(output_path=self.download_Folder)
+        QMessageBox.information(
+            self, "Download Completed", "Download has been completed successfully!")
 
     def downloader(self):
         url = self.urlEdit.text()
@@ -169,18 +162,18 @@ class Downloader(QDialog):
         stream = yt.streams.get_by_itag(251)
         if self.download_Folder:
             stream.download(output_path=self.download_Folder)
-            QMessageBox.information(self, "Download Completed", "Download has been completed successfully!")
+            QMessageBox.information(
+                self, "Download Completed", "Download has been completed successfully!")
         else:
-            QMessageBox.warning(self, "Error", "Please select download location before download starts")
-
-
+            QMessageBox.warning(
+                self, "Error", "Please select download location before download starts")
 
     def Browse(self):
         global download_Path
-        download_Path = QFileDialog.getExistingDirectory(self, "Select Directory")
+        download_Path = QFileDialog.getExistingDirectory(
+            self, "Select Directory")
         self.urthreelEdit.setText(download_Path)
         self.download_Folder = download_Path
-
 
     def showMenuDialog(self):
         dialog = QDialog(self)
@@ -191,12 +184,12 @@ class Downloader(QDialog):
         playlistButton = QPushButton('Download Playlist')
 
         self.videoQualityComboBox = QComboBox()
-        self.videoQualityComboBox.addItems(['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
+        self.videoQualityComboBox.addItems(
+            ['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
 
-        
         self.videoQualityComboBoxx = QComboBox()
-        self.videoQualityComboBoxx.addItems(['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
-
+        self.videoQualityComboBoxx.addItems(
+            ['Highest', '1080p', '720p', '480p', '360p', '240p', '144p'])
 
         layout = QVBoxLayout()
         layout.addWidget(videoButton)
@@ -211,21 +204,17 @@ class Downloader(QDialog):
         audioButton.clicked.connect(self.downloader)
         playlistButton.clicked.connect(self.downloadPlaylist)
 
-
-
-
         button_d = """
             QPushButton {
-                background-color: #f44336;
+                background-color: #000;
                 color: white;
                 font-size:11px;
-                border: 2px solid #f44336;
                 font-weight:bold;
+                border-radius: 5px;
             }
 
             QPushButton:hover {
-                background-color: white;
-                color:#f44336;
+                opacity: 0.7;
             }
         """
         button_c = """
@@ -238,13 +227,9 @@ class Downloader(QDialog):
             }
 
             QComboBox:hover {
-                color: white;
-                background-color: #008CBA;
-                font-weight:bold;
+                opacity: 0.7;
             }
         """
-
-
 
         videoButton.setStyleSheet(button_d)
 
@@ -255,24 +240,13 @@ class Downloader(QDialog):
         self.videoQualityComboBoxx.setStyleSheet(button_c)
         self.videoQualityComboBox.setStyleSheet(button_c)
 
+        videoButton.setFixedSize(300, 50)
+        audioButton.setFixedSize(300, 50)
+        playlistButton.setFixedSize(300, 50)
+        self.videoQualityComboBox.setFixedSize(300, 50)
+        self.videoQualityComboBoxx.setFixedSize(300, 50)
 
-
-        videoButton.setFixedSize(150,25)
-        audioButton.setFixedSize(150,25)
-        playlistButton.setFixedSize(150,25)
-        self.videoQualityComboBox.setFixedSize(140,25)
-        self.videoQualityComboBoxx.setFixedSize(140,25)
-
-
-
-
-    
         dialog.exec_()
-
-
-
-
-
 
 
 if __name__ == '__main__':
